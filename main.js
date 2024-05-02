7a1c2215b8120b883e31c23931315a5c86cc71ce
Nicolas Perez <npcuasquer@gmail.com>
2024-05-02 17:48:12 -0500

    Segundo commit, aqui se hicieron cambios importantes para continuar con el proyecto

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu = document.querySelector(".desktop-menu");
 const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
 const menuHamIcon = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
-const aside = document.querySelector(".product-detail");
+const shoppingCartContainer = document.querySelector("#shoppingCartContainer");
 const cardsContainer = document.querySelector(".cards-container")
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
@@ -12,16 +12,16 @@ menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 //El usuario email
 function toggleDesktopMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
   if (!isAsideClosed){
-    aside.classList.add('inactive')
+    shoppingCartContainer.classList.add('inactive')
   }
   desktopMenu.classList.toggle('inactive')
 }
 
 //El hamburguesa
 function toggleMobileMenu(){
-  const isAsideClosed = aside.classList.contains('inactive');
+  const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
 
   if (!isAsideClosed) {
     //si esta abierto, cerrarlo
@@ -40,7 +40,7 @@ function toggleCarritoAside(){
     //si esta abierto, cerrarlo
     mobileMenu.classList.add('inactive')
   }  
-  aside.classList.toggle('inactive')
+  shoppingCartContainer.classList.toggle('inactive')
 
   if (!isDesktopMenuClosed) {
     desktopMenu.classList.add('inactive')
